b597883e1e034496559059c3a89e83399b2ba04b
Shifter24 <felipenahuelviera@hotmail.com>
2023-07-03 02:17:24 -0300

    Finishing functionality for search, and loader

--- popup.js
+++ popup.js
@@ -2,29 +2,28 @@ const $extensionContainer = document.querySelector(".extension-container");
 const $getFilesBtn = document.getElementById("get-files-netsuite");
 const $querySearch = document.getElementById("query-search");
 const $bodyShowFiles = document.getElementById("body-show-files");
+const $loader = document.querySelector(".lds-ring");
+const $totalResults = document.getElementById("total-results");
 
 let netsuiteFiles = [];
 
 // Only show functionality when we are in netsuite page
-document.addEventListener('DOMContentLoaded', function ()
-{
+document.addEventListener('DOMContentLoaded', function () {
     // Get the current active tab URL
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs)
-    {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         var url = tabs[0].url;
 
         // Check if the current URL matches your desired pattern
-        (url.includes('netsuite.com/app') ?  $extensionContainer.style.display = 'block' : $extensionContainer.style.display = 'none');
+        (url.includes('netsuite.com/app') ? $extensionContainer.style.display = 'block' : $extensionContainer.style.display = 'none');
     });
 });
 
-$getFilesBtn.addEventListener("click", async () =>
-{
+$getFilesBtn.addEventListener("click", async () => {
+    debugger;
     showLoader();
-    
+
     const queryToSearch = $querySearch.value;
-    if (!queryToSearch)
-    {
+    if (!queryToSearch) {
         alert("Insert field to search!.");
         closeLoader();
         return;
@@ -32,13 +31,13 @@ $getFilesBtn.addEventListener("click", async () =>
 
     // Get all suiteScript files from 
     const { netsuiteFiles } = await chrome.storage.local.get("netsuiteFiles");
-    if (!netsuiteFiles || netsuiteFiles.length < 1){
+    if (!netsuiteFiles || netsuiteFiles.length < 1) {
         closeLoader();
         return;
     }
 
     const { netsuiteDomain } = await chrome.storage.local.get("netsuiteDomain");
-    if (!netsuiteDomain){
+    if (!netsuiteDomain) {
         closeLoader();
         return;
     }
@@ -54,46 +53,59 @@ $getFilesBtn.addEventListener("click", async () =>
     closeLoader();
 });
 
-
 // -------------------- AUXILIAR FUNCTIONS ------------------------------------
 
-function showLoader()
-{
-    document.body.style.background = "black";
+function showLoader() {
+    $loader.style.display = "flex";
 }
 
-function closeLoader()
-{
-    document.body.style.background = "white";
+function closeLoader() {
+    $loader.style.display = "none";
 }
 
-function showNetsuiteFiles(filteredFiles){
+function showNetsuiteFiles(filteredFiles) {
     $bodyShowFiles.innerHTML = "";
+    $totalResults.innerHTML = "0";
 
     filteredFiles.forEach(file => {
         $bodyShowFiles.innerHTML += `
             <tr>
                 <td>${file.name}</td>
-                <td>${file.count}</td>
-                <td><a href="${file.url}">See file</a></td>
-            </tr>
+                <td>${file.count} ${(file.count == 1 ? 'time' : 'times')}</td>
+                <td><a href="${file.url}"><svg xmlns="http://www.w3.org/2000/svg"
+                    class="icon icon-tabler icon-tabler-file-search" width="30" height="30"
+                    viewBox="0 0 24 24" stroke-width="0.8" stroke="#2c3e50" fill="none"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+                    <path d="M12 21h-5a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v4.5" />
+                    <path d="M16.5 17.5m-2.5 0a2.5 2.5 0 1 0 5 0a2.5 2.5 0 1 0 -5 0" />
+                    <path d="M18.5 19.5l2.5 2.5" />
+                    </svg></a></td>
+            </tr> 
         `
     });
+
+    $totalResults.innerHTML = filteredFiles.length;
 }
 
-async function getFilesQuery(netsuiteFiles, domain, queryToSearch)
-{
+async function getFilesQuery(netsuiteFiles, domain, queryToSearch) {
     let files = [];
 
     if (!netsuiteFiles || netsuiteFiles.length < 1) return files;
 
-    for (const file of netsuiteFiles)
-    {
+    for (const file of netsuiteFiles) {
         const fileName = file.valuesByKey.name.value;
-        const url = domain + file.valuesByKey.url.value;
+        const urlOpen = domain + file.valuesByKey.url.value;
 
-        const fileContent = await getFileContent(url);
+        const fileContent = await getFileContent(urlOpen);
         if (!fileContent) continue;
+
+        let url = new URL(urlOpen);
+        const searchParams = new URLSearchParams(url.search);
+        const id = searchParams.get("id");
+
+        url = domain + `/app/common/media/mediaitem.nl?id=${id}`
 
         // Find word inside fileContent
         var regex = new RegExp('\\b' + queryToSearch + '\\b', 'gi');
@@ -108,29 +120,25 @@ async function getFilesQuery(netsuiteFiles, domain, queryToSearch)
 }
 
 
-async function getFileContent(url)
-{
-    try
-    {
+async function getFileContent(url) {
+    try {
         const response = await fetch(url);
-        if (!response.ok)
-        {
+        if (!response.ok) {
             throw new Error("Error fetching file: " + response.status);
         }
         const content = await response.text();
         return content;
 
-    } catch (error)
-    {
+    } catch (error) {
         console.error(error);
     }
 }
 
+
 // -------------------- CHROME EXTENSION FUNCTIONS ----------------------------
 
 // Listen for content-script response in order to save gathered files
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) =>
-{
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (!message.hasOwnProperty("netsuiteFiles") || !message.hasOwnProperty("domain")) return;
 
     chrome.storage.local.set({ netsuiteFiles: message.netsuiteFiles });
